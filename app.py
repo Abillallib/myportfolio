60aa5ff5c4529bdaca7edc35bba7b505a42aee22
Abillallib <140989265+Abillallib@users.noreply.github.com>
2025-04-08 17:06:10 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,2 +1,6 @@
 import streamlit as st
 st.title("Welcome to My Data Portfolio")
+
+st.header("Project 1: AI Text to Image Generator")
+st.write("[Link to GitHub Repo](https://github.com/Abillallib/text-to-image-generator)")
+st.write("[Link to Streamlit App](https://code-generator-app-adj.streamlit.app/)")
